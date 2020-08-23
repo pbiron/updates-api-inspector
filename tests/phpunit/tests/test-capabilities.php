31606f8628c7f5c150ac2726a70de16a7395c432
Paul Biron <paul@sparrowhawkcomputing.com>
2020-08-22 19:29:47 -0600

    Capabilities: Simplify the unit tests.
    In the data provider, there is no need to wrap the user creation in a check for a static variable having a value, because the data provider is only called once :-)

--- tests/phpunit/tests/test-capabilities.php
+++ tests/phpunit/tests/test-capabilities.php
@@ -56,7 +56,8 @@ class Test_Capabilties extends Updates_API_Inspector_UnitTestCase {
 	 * @param bool    $expected True if $user should have our cap, false otherwise.
 	 */
 	public function test_capabilities( $user, $expected ) {
-		$this->assertSame( user_can( $user, Updates_API_Inspector::CAPABILITY ), $expected );
+		$actual = user_can( $user, Updates_API_Inspector::CAPABILITY );
+		$this->assertSame( $expected, $actual );
 	}
 
 	/**
@@ -72,53 +73,46 @@ class Test_Capabilties extends Updates_API_Inspector_UnitTestCase {
 	 * }
 	 */
 	public function users_data() {
-		static $init;
+		// We have to create the users here (rather than in wpSetUpBeforeClass() method, which would
+		// seem more logical),  because PHPUnit calls this data provider before calling wpSetUpBeforeClass().
+		// Add some custom roles.
+		$roles = array(
+			'no_caps'       => array(),
+			'has_meta_cap'  => array( is_multisite() ? 'manage_network_options' : 'manage_options' => true ),
+			'developer'     => array( Updates_API_Inspector::CAPABILITY => true ),
+			'non_developer' => array( Updates_API_Inspector::CAPABILITY => false ),
+		);
 
-		if ( ! $init ) {
-			// We have to create the users here (rather than in wpSetUpBeforeClass() method, which would
-			// seem more logical),  because PHPUnit calls this data provider before calling wpSetUpBeforeClass().
-			// Add some custom roles.
-			$roles = array(
-				'no_caps'       => array(),
-				'has_meta_cap'  => array( is_multisite() ? 'manage_network_options' : 'manage_options' => true ),
-				'developer'     => array( Updates_API_Inspector::CAPABILITY => true ),
-				'non_developer' => array( Updates_API_Inspector::CAPABILITY => false ),
-			);
-
-			foreach ( $roles as $role => $caps ) {
-				remove_role( $role );
-				add_role( $role, $role, $caps );
-			}
-
-			$all_roles = array_merge( array( 'administrator', 'editor', 'author', 'contributor', 'subscriber' ), array_keys( $roles ) );
-			// create users for each of the built-in roles...with the standard caps those roles have.
-			foreach ( $all_roles as $role ) {
-				self::$users[ $role ] = $this->factory->user->create_and_get( array( 'role' => $role ) );
-			}
-
-			// Add our cap to a user whose role normally wouldn't have it.
-			self::$users['no_caps_granted'] = $this->factory->user->create_and_get( array( 'role' => 'no_caps' ) );
-			self::$users['no_caps_granted']->add_cap( Updates_API_Inspector::CAPABILITY, true );
-
-			// Add what our meta-cap maps to to a user whose role normally wouldn't have it.
-			self::$users['subcriber_with_meta_cap'] = $this->factory->user->create_and_get( array( 'role' => 'subscriber' ) );
-			self::$users['subcriber_with_meta_cap']->add_cap( is_multisite() ? 'manage_network_options' : 'manage_options', true );
-
-			if ( is_multisite() ) {
-				// Make a subscriber that is also a super admin.
-				self::$users['superadmin'] = $this->factory->user->create_and_get( array( 'role' => 'subscriber' ) );
-				grant_super_admin( self::$users['superadmin']->ID );
-
-			} else {
-				self::$users['superadmin'] = $this->factory->user->create_and_get( array( 'role' => 'administrator' ) );
-			}
-
-			// Revoke our cap from a user whose role normally have it.
-			self::$users['developer_revoked'] = $this->factory->user->create_and_get( array( 'role' => 'developer' ) );
-			self::$users['developer_revoked']->add_cap( Updates_API_Inspector::CAPABILITY, false );
-
-			$init = true;
+		foreach ( $roles as $role => $caps ) {
+			remove_role( $role );
+			add_role( $role, $role, $caps );
 		}
+
+		$all_roles = array_merge( array( 'administrator', 'editor', 'author', 'contributor', 'subscriber' ), array_keys( $roles ) );
+		// create users for each of the built-in roles...with the standard caps those roles have.
+		foreach ( $all_roles as $role ) {
+			self::$users[ $role ] = $this->factory->user->create_and_get( array( 'role' => $role ) );
+		}
+
+		// Add our cap to a user whose role normally wouldn't have it.
+		self::$users['no_caps_granted'] = $this->factory->user->create_and_get( array( 'role' => 'no_caps' ) );
+		self::$users['no_caps_granted']->add_cap( Updates_API_Inspector::CAPABILITY, true );
+
+		// Add what our meta-cap maps to to a user whose role normally wouldn't have it.
+		self::$users['subcriber_with_meta_cap'] = $this->factory->user->create_and_get( array( 'role' => 'subscriber' ) );
+		self::$users['subcriber_with_meta_cap']->add_cap( is_multisite() ? 'manage_network_options' : 'manage_options', true );
+
+		if ( is_multisite() ) {
+			// Make a subscriber that is also a super admin.
+			self::$users['superadmin'] = $this->factory->user->create_and_get( array( 'role' => 'subscriber' ) );
+			grant_super_admin( self::$users['superadmin']->ID );
+		} else {
+			self::$users['superadmin'] = $this->factory->user->create_and_get( array( 'role' => 'administrator' ) );
+		}
+
+		// Revoke our cap from a user whose role normally have it.
+		self::$users['developer_revoked'] = $this->factory->user->create_and_get( array( 'role' => 'developer' ) );
+		self::$users['developer_revoked']->add_cap( Updates_API_Inspector::CAPABILITY, false );
 
 		return array(
 			// Users with built-in roles, and the caps that normally go with those roles.
